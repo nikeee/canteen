6fd654beb8d161db95a24d15f3075ee45d77c7ab
Niklas Mollenhauer <nikeee@outlook.com>
2014-09-17 02:16:51 +0200

    Implementing

--- main.ts
+++ main.ts
@@ -18,6 +18,11 @@ interface CanteenScope extends ng.IScope
 	apiUrl: string;
 
 	refreshInterval: number;
+	updateApiUrl: () => void;
+	supportedCanteens: { [name: string]: string; };
+
+	getValidWeekdays(): string[];
+	dayNames: string[];
 }
 
 class CanteenApi
@@ -38,15 +43,103 @@ class CanteenApi
 	}
 }
 
+canteen.config(["$httpProvider", $httpProvider => {
+	$httpProvider.defaults.transformResponse.push(responseData => {
+		convertDateStringsToDates(responseData);
+		return responseData;
+	});
+}]);
+
+var regexIso8601 = /^(\d{4}|\+\d{6})(?:-(\d{2})(?:-(\d{2})(?:T(\d{2}):(\d{2}):(\d{2})\.(\d{1,})(Z|([\-+])(\d{2}):(\d{2}))?)?)?)?$/;
+
+function convertDateStringsToDates(input) {
+	// Ignore things that aren't objects.
+	if (typeof input !== "object")
+		return input;
+
+	for (var key in input)
+	{
+		if (!input.hasOwnProperty(key))
+			continue;
+
+		var value = input[key];
+		var match;
+		// Check for string properties which look like dates.
+		// TODO: Improve this regex to better match ISO 8601 date strings.
+		if (typeof value === "string" && (match = value.match(regexIso8601)))
+		{
+			// Assume that Date.parse can parse ISO 8601 strings, or has been shimmed in older browsers to do so.
+			var milliseconds = Date.parse(match[0]);
+			if (!isNaN(milliseconds))
+			{
+				input[key] = new Date(milliseconds);
+			}
+		}
+		else if (typeof value === "object")
+		{
+			// Recurse into object
+			convertDateStringsToDates(value);
+		}
+	}
+}
+
 canteen.controller("CanteenCtrl", ($scope: CanteenScope, $http: ng.IHttpService, $interval: ng.IIntervalService, $q: ng.IQService) => {
 
-	$scope.apiUrl = "http://uni.holz.nu:8081/menu/wilhelmshoehe";
+	var apiBase = "http://uni.holz.nu:8081/menu/";
+
+	$scope.supportedCanteens = {
+		hopla: "Holländischer Platz",
+		wilhelmshoehe: "Wilhelmshöhe"
+	};
+
+	var fallback = $scope.supportedCanteens["wilhelmshoehe"];
+	var newApiUrl = () => {
+		var h = document.location.hash;
+		return apiBase + (h.length > 1 ? h.substring(1) || fallback : fallback);
+	};
+
+	$scope.apiUrl = newApiUrl();
+
+	$scope.updateApiUrl = () => {
+		var prev = $scope.apiUrl;
+		$scope.apiUrl = newApiUrl();
+		if(prev != $scope.apiUrl)
+			$scope.refresh();
+	};
+
+	$scope.dayNames = [
+		"Sonntag",
+		"Montag",
+		"Dienstag",
+		"Mittwoch",
+		"Donnerstag",
+		"Freitag",
+		"Samstag"
+	];
 
 	$scope.isLoading = true;
 	$scope.isError = false;
 	$scope.canteenApi = new CanteenApi($q, $http);
 	$scope.lastResult = null;
 	$scope.refreshInterval = 30 * 60 * 1000;
+
+	$scope.getValidWeekdays = () => {
+		if($scope.isLoading || $scope.isError)
+			return [];
+
+		//var menuDays = new Array<Date>();
+		var loopTime = $scope.lastResult.menu.validity.from.getTime();
+		var endTime = $scope.lastResult.menu.validity.until.getTime();
+		var arr = new Array<string>();
+		for(; loopTime <= endTime; loopTime += 86400000)
+		{
+			arr.push($scope.dayNames[(new Date(loopTime)).getDay()]);
+			//menuDays.push(new Date(loopTime));
+		}
+		//var arr = new Array<string>(menuDays.length);
+		//for(var i = 0; i < menuDays.length; ++i)
+		return arr;
+	};
 
 	$scope.refresh = () => {
 		$scope.isLoading = true;
@@ -70,4 +163,6 @@ canteen.controller("CanteenCtrl", ($scope: CanteenScope, $http: ng.IHttpService,
 
 	$scope.refresh();
 	$interval(() => $scope.refresh(), $scope.refreshInterval);
+
+	window.addEventListener("hashchange",() => $scope.updateApiUrl(), false);
 });
